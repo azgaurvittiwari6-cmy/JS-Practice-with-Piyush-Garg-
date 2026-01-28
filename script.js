11e2455a0307b63da2a9a39a9a8bab687f7d57c7
azgaurvittiwari6-cmy <azgaurvittiwari6@gmail.com>
2026-01-28 19:31:29 +0530

    Updated 13.html

---
+++ script.js
@@ -0,0 +1,9 @@
+function showTime(){
+    const  currentTime =new Date()
+    const time =`${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`
+    document.getElementById("time").innerText = time;
+    
+}
+
+setInterval(showTime,1000)
+                                                                                                                                                                                  
